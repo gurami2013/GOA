b61412af2f4260b12f97630fa4e77c443bd2feff
gurami2013 <khurtilavagurami13@gmail.com>
2024-08-30 13:43:37 -0700

    second commit

---
+++ level 31/homework/main(4).py
@@ -0,0 +1,2 @@
+num = ["apple","banana","cherry","data","legvi","grape"]
+print(num[::-1])
---
+++ level 31/homework/main(5).py
@@ -0,0 +1,3 @@
+num = ["apple","banana","cherry","data","legvi","grape"]
+num[2:4]=['kiwi','cacxvi']
+print(num)
